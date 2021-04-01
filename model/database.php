610288ef9f8da7c970b3bd643cdb97b49d23b50c
Eugene <e_kramer2@mail.fhsu.edu>
2021-03-31 21:53:15 -0700

    Session Cookies Project - Update JAWS DB

--- model/database.php
+++ model/database.php
@@ -24,10 +24,15 @@
     try {
         //local development server connection
         //if using a $password, add it as 3rd parameter
-        $db = new PDO($dsn, $username);
+        //$db = new PDO($dsn, $username);
+
 
         // Heroku connection
         //$db = new PDO($dsn, $username, $password);
+
+        $db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+        // set the PDO error mode to exception
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
         $error = "Database Error: ";
         $error .= $e->getMessage();
